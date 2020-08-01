5c704b1189b709e2b59097e5daae48a7ad1d86d2
Rani Khoury <ranik@campus.technion.ac.il>
2020-08-01 18:03:36 +0300

    Add class with shaddah

--- DiacriticDataset.py
+++ DiacriticDataset.py
@@ -57,7 +57,7 @@ class DiacriticDataset(Dataset):
     def prepare_dataset(self):
         data = {}
         counter = 0
-        with open(self.file) as file:
+        with open(self.file,encoding='utf8') as file:
             for line in file:
                 letter_ids = []
                 diacritic_ids = []
@@ -88,3 +88,83 @@ class DiacriticDataset(Dataset):
                 counter += 1
         return data
 
+class DiacriticDatasetShaddah(Dataset):
+    def __init__(self,dataset_path,letter_to_id_path,id_to_letter_path,diacritic_to_id_path,id_to_diacritic_path,word_to_id_path,id_to_word_path):
+        
+        self.file = dataset_path
+        
+        letter_to_id_file= open(letter_to_id_path, 'rb')
+        self.letter_to_id = pickle.load(letter_to_id_file)
+        letter_to_id_file.close()
+        
+        id_to_letter_file = open(id_to_letter_path, 'rb')
+        self.id_to_letter = pickle.load(id_to_letter_file)
+        id_to_letter_file.close()
+        
+        diacritic_to_id_file= open(diacritic_to_id_path, 'rb')
+        self.diacritic_to_id = pickle.load(diacritic_to_id_file)
+        diacritic_to_id_file.close()
+        
+        id_to_diacritic_file = open(id_to_diacritic_path, 'rb')
+        self.id_to_diacritic = pickle.load(id_to_diacritic_file)
+        id_to_diacritic_file.close()
+        
+        word_to_id_file= open(word_to_id_path, 'rb')
+        self.word_to_id = pickle.load(word_to_id_file)
+        word_to_id_file.close()
+        
+        id_to_word_file = open(id_to_word_path, 'rb')
+        self.id_to_word = pickle.load(id_to_word_file)
+        id_to_word_file.close()
+        
+        self.data = self.prepare_dataset()
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self,index):
+        return self.data[index]
+    
+    def prepare_dataset(self):
+        data = {}
+        counter = 0
+        with open(self.file,encoding='utf-8') as file:
+            for line in file:
+                letter_ids = []
+                diacritic_ids = []
+                word_ids = []
+                letters, diacritics =araby.separate(line)
+                letters = letters[0:-1]
+                words = araby.tokenize(line)[0:-1]
+                diacritics = diacritics[0:-1]
+                index = 0
+                for letter in letters:
+                    if (letter == '\n') or (letter == '\u200f'):
+                        continue
+                    if (letter =='ّ'):
+                        if diacritics[index] != 'ـ':
+                            diacritic_ids[-1] = self.diacritic_to_id[letter+diacritics[index]]
+                            
+                        else:
+                            diacritic_ids[-1] = self.diacritic_to_id[letter]
+                        
+                            
+                    else:
+                        letter_ids.append(self.letter_to_id[letter])
+                        if letter == " ":
+                            diacritic_ids.append(self.diacritic_to_id['space'])
+                            
+                        else:
+                            diacritic_ids.append(self.diacritic_to_id[diacritics[index]])
+                            
+                    index += 1
+                    
+                for word in words:
+                    word_ids.append(self.word_to_id[word])
+
+                instance = (torch.tensor(letter_ids,dtype=torch.long,requires_grad=False),
+                           torch.tensor(diacritic_ids,dtype=torch.long,requires_grad=False),
+                           torch.tensor(word_ids,dtype=torch.long,requires_grad=False))
+                data[counter] = instance
+                counter += 1
+        return data
