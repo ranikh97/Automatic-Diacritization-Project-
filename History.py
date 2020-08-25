e6b3c03b57fdaadbad900513369d6d4f8e934137
eliast9 <38985331+eliast9@users.noreply.github.com>
2020-08-25 21:17:08 +0300

    Added History with position in word

--- History.py
+++ History.py
@@ -1,30 +1,84 @@
+import pickle
+
+with open('Dataset_with_shaddah/letter_to_id.pickle', 'rb') as file:
+    letterMap = pickle.load(file)
+
+
 class History:
-    def __init__(self, prevLetter1, prevLetter2, nextLetter1, nextLetter2, prevWord, sentence, position):
-        self.prevLetter1 = prevLetter1
-        self.prevLetter2 = prevLetter2
-        self.nextLetter1 = nextLetter1
-        self.nextLetter2 = nextLetter2
-        self.prevWord = prevWord
+    def __init__(self, prevLable1, prevLable2, sentence, position):
+        # prevLetter, nextLetter, prevWord, currWord
+        self.prevLabel1 = prevLable1
+        self.prevLabel2 = prevLable2
         self.sentence = sentence
         self.position = position
+        self.current_word, self.prev_word, self.position_in_word = self.__parse_words(sentence, position)
+        self.prevLetter = sentence[position-1]
+        self.nextLetter = sentence[position+1]
 
-    def get_prev_letter1(self):
-        return self.prevLetter1
+    def __parse_words(self, sentence, position):
+        prev_space_position = None
+        word_begin_position = 0
+        position_in_word = 0
 
-    def get_prev_letter2(self):
-        return self.prevLetter2
+        i = 0
+        if sentence[0] == letterMap[' ']:
+            i = 1
 
-    def get_next_letter1(self):
-        return self.prevLetter1
+        while i < position:
+            if sentence[i] == letterMap[' ']:
+                prev_space_position = word_begin_position
+                word_begin_position = i
+            i += 1
 
-    def get_next_letter2(self):
-        return self.prevLetter2
+        if word_begin_position == 0 and sentence[0] != letterMap[' ']:
+            word_begin_position = -1
+            position_in_word = position
+        else:
+            position_in_word = position - word_begin_position - 1
+
+        word_end_position = len(sentence) - 1
+        while i < len(sentence):
+            if sentence[i] == letterMap[' ']:
+                word_end_position = i
+                break
+            i += 1
+
+        if i == len(sentence):
+            word_end_position = len(sentence)
+
+        current_word = sentence[word_begin_position+1:word_end_position]
+
+        if word_begin_position == -1:
+            prev_word = []
+        else:
+            prev_word = sentence[prev_space_position:word_begin_position]
+
+        return current_word, prev_word, position_in_word
+
+    def get_prev_letter(self):
+        return self.prevLetter
+
+    def get_next_letter(self):
+        return self.prevLetter
 
     def get_prev_word(self):
-        return self.prevWord
+        return self.prev_word
+
+    def get_curr_word(self):
+        return self.current_word
 
     def get_sentence(self):
         return self.sentence
 
     def get_position(self):
         return self.position
+
+    def get_position_in_word(self):
+        return self.position_in_word
+
+
+sentence = [2, 11, 12, 3, 2, 1, 10, 21, 2, 112, 412]
+histTest = History('a', 'u', sentence, 2)
+print("Prev Word: ", histTest.get_prev_word())
+print("Curr Word: ", histTest.get_curr_word())
+print("Position in Word: ", histTest.get_position_in_word())
